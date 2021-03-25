677552d2e0babff66abb3220cf3861bf0a6a1bd8
Najeeb Arif <NajeebArif786@outlook.com>
2021-03-26 01:04:49 +0530

    Adding orbit camera and relative movement scripts

---
+++ Assets/Scripts/RelativeMotion.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelativeMotion : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    void Update() {
+        Vector3 movement = Vector3.zero;
+        float horInput = Input.GetAxis("Horizontal");
+        float vertInput = Input.GetAxis("Vertical");
+        if (horInput != 0 || vertInput != 0) {
+            movement.x = horInput;
+            movement.z = vertInput;
+            Quaternion tmp = target.rotation;
+            target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+            movement = target.TransformDirection(movement);
+            target.rotation = tmp;
+            transform.rotation = Quaternion.LookRotation(movement);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
