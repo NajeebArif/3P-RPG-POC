9d42822a14dd6acf584d34ad65e989685a4b9eb1
Najeeb Arif <NajeebArif786@outlook.com>
2021-03-26 01:45:30 +0530

    Adding prototyping assets

--- Assets/Scripts/RelativeMotion.cs
+++ Assets/Scripts/RelativeMotion.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(CharacterController))]
 public class RelativeMotion : MonoBehaviour
 {
     [SerializeField] private Transform target;
     public float rotSpeed = 15f;
+    public float moveSpeed = 6f;
+
+    private CharacterController _charController;
+    // Start is called before the first frame update
+    void Start() {
+        _charController =  GetComponent<CharacterController>();
+    }
 
     void Update() {
         Vector3 movement = Vector3.zero;
         float horInput = Input.GetAxis("Horizontal");
         float vertInput = Input.GetAxis("Vertical");
         if (horInput != 0 || vertInput != 0) {
-            movement.x = horInput;
-            movement.z = vertInput;
+            movement.x = horInput * moveSpeed;
+            movement.z = vertInput * moveSpeed;
+            movement = Vector3.ClampMagnitude(movement, moveSpeed);
             Quaternion tmp = target.rotation;
             target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
             movement = target.TransformDirection(movement);
@@ -21,11 +29,9 @@ public class RelativeMotion : MonoBehaviour
             Quaternion direction = Quaternion.LookRotation(movement);
             transform.rotation = Quaternion.Lerp(transform.rotation,direction, rotSpeed*Time.deltaTime);
         }
+        movement *= Time.deltaTime;
+        _charController.Move(movement);
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
 }
